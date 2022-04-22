0ffc4701ee6d259faeb43e14a27fbe8e3bb29737
KozlowskiSzymon <Szymonztn7?yj>
2022-04-22 15:44:10 +0200

    base url from environments

--- src/app/components/book/service/book.service.ts
+++ src/app/components/book/service/book.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Book} from "../model/book";
+import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class BookService {
 
   constructor(private http: HttpClient) { }
-  basicUrl = 'http://localhost:8080/book/'
+  basicUrl = environment.API_URL;
+  urlSuff = '/book/'
 
   getAllBooks() {
-    return this.http.get<Book[]>(this.basicUrl + 'all');
+    return this.http.get<Book[]>(this.basicUrl + this.urlSuff + 'all');
   }
 
   getById(id: number) {
-    return this.http.get<Book>(this.basicUrl + id);
+    return this.http.get<Book>(this.basicUrl + this.urlSuff + id);
   }
 
   save(book: Book) {
-    return this.http.post(this.basicUrl + 'save', book);
+    return this.http.post(this.basicUrl + this.urlSuff + 'save', book);
   }
 
   remove(book: Book) {
-    return this.http.delete(this.basicUrl + book.id);
+    return this.http.delete(this.basicUrl + this.urlSuff + book.id);
   }
 }
